9ce439c2b2b3e653c5d1840713829e88cf0c16fe
Mohye <dragonsinmeliodas777@gmail.com>
2024-09-11 20:06:04 +0300

    install jwt configuration

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,12 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        'jwt' => [
+            'driver' => 'jwt',
+            'provider' => 'users',
+            'hash' => false,
+        ],
     ],
 
     /*
