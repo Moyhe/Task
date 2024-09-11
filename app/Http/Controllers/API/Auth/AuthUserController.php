0d391514f36ac14c883aa7fd3fd56f62ad5d2e74
Mohye <dragonsinmeliodas777@gmail.com>
2024-09-11 21:44:26 +0300

    implement user authentication

--- app/Http/Controllers/API/Auth/AuthUserController.php
+++ app/Http/Controllers/API/Auth/AuthUserController.php
@@ -3,9 +3,72 @@
 namespace App\Http\Controllers\API\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\Password;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthUserController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' =>  ['required', 'string', 'unique:users', 'max:255', 'email'],
+            'password' => ['required', Password::defaults()->min(8), 'confirmed']
+        ]);
+
+        if ($validator->fails()) {
+
+            return Response::json($validator->errors(), 400);
+        }
+
+        User::create([
+            'name' => request('name'),
+            'email' => request('email'),
+            'password' => Hash::make(request('password')),
+        ]);
+
+
+        $user = User::query()->firstOrFail();
+        $token = JWTAuth::fromUser($user);
+
+
+        return Response::json(compact('user', 'token'), 200);
+    }
+
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+
+            'email' =>  ['required', 'string', 'max:255', 'email'],
+            'password' => ['required', Password::defaults()->min(8)]
+
+        ]);
+
+        if ($validator->fails()) {
+
+            return Response::json($validator->errors(), 400);
+        }
+
+        $credentials = $request->only('email', 'password');
+
+        try {
+            if (!$token = JWTAuth::attempt($credentials)) {
+
+                return Response::json(['error' => 'invalid user name and password'], [401]);
+            }
+        } catch (JWTException $e) {
+
+            return Response::json(['error' => 'could not create token'], [500]);
+        }
+
+        return response()->json(compact('token'), 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\API\Auth\AuthUserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:jwt');
+
+
+
+Route::middleware('guest')->group(function () {
+
+    Route::post('/user/register', [AuthUserController::class, 'register'])->name('api.register');
+    Route::post('/user/login', [AuthUserController::class, 'login'])->name('api.login');
+});
